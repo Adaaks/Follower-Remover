9d30e3615ff4b36dd6dc24a4b8e95f5b4da99d32
Adaaks <adampotempa78@gmail.com>
2022-04-07 00:35:05 +0100

    version 2 (added renewing xcsrf token)

--- Follower Remover.py
+++ Follower Remover.py
@@ -9,8 +9,7 @@ try:
     
 except ImportError:
     print("[ERROR] Failed to import some modules, install the required modules listed:\n- requests\n- colorama\n- configparser\n\nThese can be installed via going to the cmd, and typing pip install (moduleName)")
-    time.sleep(5)
-    quit()
+    input()
     
 config = configparser.ConfigParser()
 config.read_file(open(r"Config.ini"))
@@ -44,8 +43,7 @@ try:
     print(f"{Fore.GREEN}[Authentication] Logged in as {getuser4}")
 except:
     print(f"{Fore.RED}[ERROR] Your cookie is invalid")
-    time.sleep(3)
-    quit()
+    input()
 
 friends2 = True
 friendscount = 0
@@ -64,8 +62,7 @@ val = True
 if count3 == 0:
     print(Fore.RED + "[ERROR] Your followers have been removed already")
     val = False
-    time.sleep(5)
-    quit()
+    input()
 
 times = 0
 
@@ -75,6 +72,8 @@ while val == True:
         global times
         global count
         global val
+        global cookie
+        global session
         getfollower = session.get(f"https://friends.roblox.com/v1/users/{getuser3}/followers")
         followes = getfollower.json()
         followers2 = followes['data'][0]['id']
@@ -84,34 +83,57 @@ while val == True:
                 write = file.writelines(str(followers2)+'\n')
                 file.close()
                 pass
+        counth = session.get(f"https://friends.roblox.com/v1/users/{getuser3}/followers/count")
+        countb = counth.json()
+        countq = countb['count']
+
+        
         block = session.post(f"https://accountsettings.roblox.com/v1/users/{followers2}/block")
         unblock = session.post(f"https://accountsettings.roblox.com/v1/users/{followers2}/unblock")
         
-        
-        if block.status_code == 401 and unblock.status_code == 401:
+
+        if block.status_code == 400:
+            print(f"{Fore.RED}[RATELIMIT] Too much requests being sent (codes: {block.status_code}/{unblock.status_code})")
+            
+        elif unblock.status_code == 400:
+            unblockx = session.post(f"https://accountsettings.roblox.com/v1/users/{followers2}/unblock")
+
+        elif block.status_code == 401 and unblock.status_code == 401:
             print(f"{Fore.RED}[AUTH] Token Validation Failed (codes: {block.status_code}/{unblock.status_code})")
             val = False
             print(f"{Fore.YELLOW}[INFO] Please restart the program, with a new cookie")
-            time.sleep(1000000)
-        
-        elif block.status_code == 400 or unblock.status_code == 400:
-            print(f"{Fore.RED}[RATELIMIT] Too much requests being sent (codes: {block.status_code}/{unblock.status_code})")
-        
-        
-        else: 
+            input()
+            
+        elif block.status_code == 403 and unblock.status_code == 403:
+            print(f"{Fore.RED}[AUTH] Renewing X-CSRF token") 
+            session = requests.Session()
+            session.cookies[".ROBLOSECURITY"] = cookie
+            # send first request
+            req = session.post(
+                url="https://auth.roblox.com/"
+            )
+
+            if "X-CSRF-Token" in req.headers:  # check if token is in response headers
+                session.headers["X-CSRF-Token"] = req.headers["X-CSRF-Token"]  # store the response header in the session
+
+            # send second request
+            req2 = session.post(
+                url="https://auth.roblox.com/"
+            )
+
+        elif block.status_code == 200 and unblock.status_code == 200: 
             count = session.get(f"https://friends.roblox.com/v1/users/{getuser3}/followers/count")
             count5 = count.json()
             count3 = count5['count']
             times+=1
-            print(f"{Fore.BLUE}[Progress] {times}/{count3}")
-    program()
+            print(f"{Fore.BLUE}[Progress] {times}/{count3} ({block.status_code}/{unblock.status_code})")
 
+    program()
 
     if times >= count3:
         print(f"{Fore.GREEN}[SUCCESS] Removed all followers")
         print(f"{Fore.YELLOW}[INFO] You may want to check if you have any friends to add back in: FriendsToAddBack.txt")
-        time.sleep(10)
-        quit()
+        input()
     
         
 
